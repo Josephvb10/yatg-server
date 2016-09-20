60b5d873c025d8898f31601a1f0046dcf5090d73
Jimena Salas Alfaro <jimenamaria598@gmail.com>
2016-09-19 20:05:19 -0600

    mierda

--- src/Malla/Main.java
+++ src/Malla/Main.java
@@ -1,12 +1,12 @@
 package Malla;
 import Malla.*;
 
-public class Main {
+public class MainTron {
 
 	public static void main(String[] args) {
 		// Esto es un comentario para probar
 		// Hola mundo
-		MatrizEnlazada malla = new MatrizEnlazada(4,5);
+		LinkedMatrix malla = new LinkedMatrix(4,5);
 		
 
 
