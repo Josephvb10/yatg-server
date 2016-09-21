2d367864fbf36a1df0efbda140c2bc479fb094cc
gsegura96 <gsegura96@openmailbox.org>
2016-09-20 22:42:58 -0600

    Creada la clase OutputServer, junto con sus gets, sets y el metodo sentToAll

---
+++ src/Comunication/OutputServer.java
@@ -0,0 +1,77 @@
+package Comunication;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+
+public class OutputServer{
+	
+	private int port;
+	private ArrayList<String> listeners;
+	private OutputMessage outputMessage;
+	private int maxTimeOut=10;
+	
+	public OutputServer(int port, ArrayList<String> listeners, OutputMessage outputMessage) {
+		super();
+		this.port = port;
+		this.listeners = listeners;
+		this.outputMessage = outputMessage;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	public ArrayList<String> getListeners() {
+		return listeners;
+	}
+
+	public void setListeners(ArrayList<String> listeners) {
+		this.listeners = listeners;
+	}
+
+	public OutputMessage getOutputMessage() {
+		return outputMessage;
+	}
+
+	public void setOutpuMessage(OutputMessage outputMessage) {
+		this.outputMessage = outputMessage;
+	}
+
+	public int getMaxTimeOut() {
+		return maxTimeOut;
+	}
+
+	public void setMaxTimeOut(int maxTimeOut) {
+		this.maxTimeOut = maxTimeOut;
+	}
+	
+	public void sendToAll(OutputMessage message){
+	ListIterator<String> listenersIterator= listeners.listIterator();
+		while(listenersIterator.hasNext()){
+			try {
+				Socket listenerSocket = new Socket(listenersIterator.next(), port);
+				DataOutputStream outputFlow = new DataOutputStream(listenerSocket.getOutputStream());
+				outputFlow.writeUTF(outputMessage.getJson())
+				outputFlow.close();
+				listenerSocket.close();
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				//e.printStackTrace();
+				System.out.println(e.getMessage());
+
+			}
+			
+		}
+	}
+
+
+}
