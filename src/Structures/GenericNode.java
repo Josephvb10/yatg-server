a13b8b61e7b0dd1bbe96d6a0535048525931964f
gsegura96 <gsegura96@openmailbox.org>
2016-09-23 00:32:15 -0600

    Creada la clase GenericNode

---
+++ src/Structures/GenericNode.java
@@ -0,0 +1,35 @@
+package Structures;
+
+public class GenericNode<T> {
+	private T data;
+	private GenericNode<T> next;
+	
+	public GenericNode(){
+		data=null;
+		next=null;
+	}
+	
+	public GenericNode(T data) {
+		super();
+		this.data = data;
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+
+	public GenericNode<T> getNext() {
+		return next;
+	}
+
+	public void setNext(GenericNode<T> next) {
+		this.next = next;
+	}
+	 
+	
+	
+}
