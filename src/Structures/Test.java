bbf621b1b476321aecdc4a20604ec9478e016e3a
gsegura96 <gsegura96@openmailbox.org>
2016-09-25 14:35:32 -0600

    Editado para pruebas

--- src/Structures/Test.java
+++ src/Structures/Test.java
@@ -1,18 +1,23 @@
 package Structures;
 
+import java.util.PriorityQueue;
+
 public class Test {
 
 	public static void main(String[] args) {
-		
-		// TODO Auto-generated method stub
-		GenericLinkedList<String> nueva_lista = new GenericLinkedList<>();
-		nueva_lista.add("hola1");
-		nueva_lista.add("hola2");
-		nueva_lista.add("hola3");
-		nueva_lista.insertAtBeggining("hola0");
-		System.out.println(nueva_lista.getAtPosition(2));
-		System.out.println(nueva_lista.getAtPosition(0));		
-		
+		PriorityQueue<Carro> colaPrioridad = new PriorityQueue<>();
+		Carro carro1 = new Carro(9090, "toyota", Carro.Importancia.patrulla);
+		Carro carro2 = new Carro(1212, "nissan", Carro.Importancia.taxi);
+		Carro carro3 = new Carro(6767, "Hummer", Carro.Importancia.uber);
+		Carro carro4 = new Carro(2323, "jeep", Carro.Importancia.patrulla);
+		colaPrioridad.add(carro1);
+		colaPrioridad.add(carro2);
+		colaPrioridad.add(carro3);
+		colaPrioridad.add(carro4);
+		while(colaPrioridad.size()>0){
+			System.out.println(colaPrioridad.poll().getMarca());
+		}
+		System.out.println(colaPrioridad);
 	}
 
 }
