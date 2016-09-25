648cfc5c968be009a73c384c52884105e7ba2b28
gsegura96 <gsegura96@openmailbox.org>
2016-09-25 16:32:11 -0600

    Corregido problema que generaba un error de puntero, debido a que no de pasaba el atributo Item cuando se creaba el head de la cola

--- src/Structures/ItemsPriorityQueue.java
+++ src/Structures/ItemsPriorityQueue.java
@@ -1,5 +1,43 @@
 package Structures;
 
-public class PriorityQueue {
+public class ItemsPriorityQueue extends GenericLinkedList<Item>{
+	@Override
+	public void add(Item newItem){
+		GenericNode<Item> newNode= new GenericNode<>(newItem);
+		add(newNode);
+	}
+	@Override
 
+	public void add(GenericNode<Item> newNode){
+		
+		if(isEmpty()){
+			setHead(newNode);
+		}
+		else{
+			
+			GenericNode<Item> current = getHead();
+			while(current.getNext()!=null){
+				System.out.println(current.getNext().getData().compareTo(newNode.getData()));
+				if(current.getNext().getData().compareTo(newNode.getData()) >= 1){
+				break;
+				}
+				else{
+					current = current.getNext();
+				}
+			}
+			newNode.setNext(current.getNext());
+			current.setNext(newNode);
+			
+			
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
