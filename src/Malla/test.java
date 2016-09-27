dbfa03eca7720908148b263a1e621cc9ab940c3d
Jimena Salas Alfaro <jimenamaria598@gmail.com>
2016-09-27 17:11:05 -0600

    Prueba para imprimir los items en la matriz

--- src/Malla/test.java
+++ src/Malla/test.java
@@ -9,20 +9,21 @@ public class test {
 		// TODO Auto-generated method stub		
 		LinkedMatrix malla = new LinkedMatrix(10,10);
 		Item item1 = new Item(ItemType.bomb, 1, 0);
-		Item item2 = new Item(ItemType.tronTrail, 0, 1, true, Player.player3);
-		Item item3 = new Item(ItemType.tronTrail, 0, 2, false, Player.player3);
-		Item item4 = new Item(ItemType.tronTrail, 1, 1, true, Player.player2);
-		Item item5 = new Item(ItemType.tronTrail, 1, 2, false, Player.player2);
-		Item item6 = new Item(ItemType.fuel, 0, 3);
+		Item item2 = new Item(ItemType.shield, 2, 1 );
+		Item item3 = new Item(ItemType.turbo, 3, 2);
+		Item item4 = new Item(ItemType.bomb, 1, 1);
+		Item item5 = new Item(ItemType.increaseTail, 1, 2);
+		Item item6 = new Item(ItemType.fuel, 5, 3);
 		Item item7 = new Item(ItemType.increaseTail, 1, 3);
-		/*
+		
+		
 		malla.setNodeItem(item1);
 		malla.setNodeItem(item2);
 		malla.setNodeItem(item3);
 		malla.setNodeItem(item4);
 		malla.setNodeItem(item5);
 		malla.setNodeItem(item6);
-		malla.setNodeItem(item7);*/
+		malla.setNodeItem(item7);
 
 		
 		//malla.resetNodeItem(item6);
@@ -30,35 +31,18 @@ public class test {
 		Troncycle player1 = new	Troncycle(Player.player1, 0, 0);
 		player1.setCurrentDirection(Direction.right);
 		malla.updatePlayer(player1);
-		System.out.println(malla.getNodo(0, 0).getItem());
-		System.out.println(malla.getNodo(0, 1).getItem());
-		System.out.println(malla.getNodo(0, 2).getItem());
-		System.out.println(malla.getNodo(0, 3).getItem());
+		
 		malla.updatePlayer(player1);
-		System.out.println(malla.getNodo(0, 0).getItem());
-		System.out.println(malla.getNodo(0, 1).getItem());
-		System.out.println(malla.getNodo(0, 2).getItem());
-		System.out.println(malla.getNodo(0, 3).getItem());
+		
 		malla.updatePlayer(player1);
-		System.out.println(malla.getNodo(0, 0).getItem());
-		System.out.println(malla.getNodo(0, 1).getItem());
-		System.out.println(malla.getNodo(0, 2).getItem());
-		System.out.println(malla.getNodo(0, 3).getItem());
+		
 		malla.updatePlayer(player1);
-		System.out.println(malla.getNodo(0, 0).getItem());
-		System.out.println(malla.getNodo(0, 1).getItem());
-		System.out.println(malla.getNodo(0, 2).getItem());
-		System.out.println(malla.getNodo(0, 3).getItem());
+		
 		malla.updatePlayer(player1);
-		System.out.println(malla.getNodo(0, 0).getItem());
-		System.out.println(malla.getNodo(0, 1).getItem());
-		System.out.println(malla.getNodo(0, 2).getItem());
-		System.out.println(malla.getNodo(0, 3).getItem());
+		
 		malla.updatePlayer(player1);
-		System.out.println(malla.getNodo(0, 0).getItem());
-		System.out.println(malla.getNodo(0, 1).getItem());
-		System.out.println(malla.getNodo(0, 2).getItem());
-		System.out.println(malla.getNodo(0, 3).getItem());
+		
+		malla.displayList();
 
 
 		
