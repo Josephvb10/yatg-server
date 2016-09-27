8f17be80c516d5250ac1088dcf1cafa2fc2b0983
gsegura96 <gsegura96@openmailbox.org>
2016-09-27 02:51:04 -0600

    Fusionado Item con TronTail

--- src/Structures/Item.java
+++ src/Structures/Item.java
@@ -2,36 +2,29 @@ package Structures;
 
 import java.util.Random;
 
+//import Structures.TronTail.Player;
+
 public class Item implements Comparable<Item>{
 	private Type type;
 	private int indexI;
 	private int indexJ;
 	private int value;
+	private boolean isHead;
+	private Player owner;
+	
 
 	
+	public Item(Type type) {
+		super();
+		setType(type);
+	}
 	public Item(Type type,int indexI, int indexJ) {
 		super();
 		setType(type);
-		this.indexI = indexI;
-		this.indexJ = indexJ;
+		this.setIndexI(indexI);
+		this.setIndexJ(indexJ);
 	}
 	
-	public int getIndexI() {
-		return indexI;
-	}
-
-	public void setIndexI(int indexI) {
-		this.indexI = indexI;
-	}
-
-	public int getIndexJ() {
-		return indexJ;
-	}
-
-	public void setIndexJ(int indexJ) {
-		this.indexJ = indexJ;
-	}
-
 	public int getValue() {
 		return value;
 	}
@@ -56,7 +49,7 @@ public class Item implements Comparable<Item>{
 	}
 	
 	public static enum Type {
-		bomb(1), fuel(2), increaseTail(3), shield(4), turbo(5);
+		bomb(1), fuel(2), increaseTail(3), shield(4), turbo(5),tronHead(6), tronTail(7);
 		private int value;
 		private Type(int value) {
 			this.value = value;
@@ -72,4 +65,40 @@ public class Item implements Comparable<Item>{
 		// TODO Auto-generated method stub
 		return this.type.compareTo(o.getType());
 	}
+	public static enum Player {
+		player1, player2,player3,player4,bot;
+	}
+
+	public int getIndexI() {
+		return indexI;
+	}
+
+	public void setIndexI(int indexI) {
+		this.indexI = indexI;
+	}
+
+	public int getIndexJ() {
+		return indexJ;
+	}
+
+	public void setIndexJ(int indexJ) {
+		this.indexJ = indexJ;
+	}
+
+	public boolean isHead() {
+		return isHead;
+	}
+
+	public void setHead(boolean isHead) {
+		this.isHead = isHead;
+	}
+
+	public Player getOwner() {
+		return owner;
+	}
+
+	public void setOwner(Player owner) {
+		this.owner = owner;
+	}
+	
 }
