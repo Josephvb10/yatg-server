146ad7bf2fc0ab1074149c212f74bdca1547d9b6
gsegura96 <gsegura96@openmailbox.org>
2016-10-01 00:41:30 -0600

    pruebas enviar json

--- src/tests/TestRunner.java
+++ src/tests/TestRunner.java
@@ -2,6 +2,8 @@
 package tests;
 
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.PriorityQueue;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
@@ -55,6 +57,14 @@ public class TestRunner {
 
 		System.out.println(playerClon.getCurrentDirection());
 		
+		InetAddress IP = null;
+		try {
+			IP = InetAddress.getLocalHost();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("IP of my system is := "+IP.getHostAddress());
 		
 	}
 
