b4b3a8ce114527151f8abdbdc472a813448d8bd8
gsegura96 <gsegura96@openmailbox.org>
2016-10-03 23:33:33 -0600

    Ya no hace prints de la direccion actual

--- src/Structures/Malla/LinkedMatrix.java
+++ src/Structures/Malla/LinkedMatrix.java
@@ -172,7 +172,7 @@ public class LinkedMatrix {
 			Item first = current.getData();
 			int indexI = first.getIndexI();
 			int indexJ = first.getIndexJ();
-			System.out.println(player.getCurrentDirection());
+			//System.out.println(player.getCurrentDirection());
 
 			Nodo nodoToCheck = getNextNode(indexI, indexJ, player.getCurrentDirection());
 			Item itemToCheck = nodoToCheck.getItem();
@@ -262,7 +262,7 @@ public class LinkedMatrix {
 	public Nodo getNextNode(int indexI, int indexJ, Direction direction) {
 		Nodo currentNodo = this.getNodo(indexI, indexJ);
 		Nodo result = null;
-		System.out.println(direction);
+		//System.out.println(direction);
 		switch (direction) {
 		case down:
 			result = currentNodo.getDown();
